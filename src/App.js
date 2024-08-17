225bdcb4d1dcda998bc9e20fb403659e7b4884b2
Kamran <mkamran4786@gmail.com>
2024-08-18 00:00:38 +0500

    - add finger print code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import FingerprintJS from '@fingerprintjs/fingerprintjs';
 
 function App() {
+    const [data, setData] = useState({});
+    const [ipAddress, setIpAddress] = useState();
+    const [time, setTime] = useState();
+    const [showOutput, setShowOutput] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    // create and set the fingerprint as soon as
+    // the component mounts
+    const getFingerPrint = async() => {
+        setShowOutput(true);
+        setLoading(true);
+        try {
+            const startTime = Date.now()
+            const fp = await FingerprintJS.load();
+
+            const response = await fp.get();
+            if(!response) setError(true);
+            setLoading(false);
+
+
+            setData(response);
+
+            fetch('https://api.ipify.org?format=json')
+                .then(response => response.json())
+                .then(data => {
+                    const totalTime = Date.now() - startTime;
+                    setTime(totalTime);
+                    setIpAddress(data.ip);
+                })
+                .catch(error => console.error('Error fetching IP:', error));
+        }
+        catch (e) {
+            setError(true);
+            setLoading(false);
+        }
+    };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getFingerPrint}>
+        Get Finger Print
+      </button>
+        {showOutput && <div>
+            {loading ? <div>Loading...</div> : <div className="out-put">
+                <br/>
+                <div className="info">
+                    <div className="title">Visitor Identifier Hash:</div>
+                    <div className="hash">{data.visitorId}</div>
+                </div>
+                <div className="separator"/>
+                <div className="info">
+                    <div className="title">Time took to get the identifier:</div>
+                    <div className="value">{time}ms</div>
+                </div>
+                <div className="info">
+                    <div className="title">Confidence Score:</div>
+                    <div className="value">{data.confidence?.score}</div>
+                </div>
+                <div className="info">
+                    <div className="title">User Agent:</div>
+                    <div className="value">{navigator?.userAgent}</div>
+                </div>
+
+                <div className="info">
+                    <div className="title">IP Address:</div>
+                    <div className="value">{ipAddress}</div>
+                </div>
+            </div>}
+        </div>}
     </div>
   );
 }
