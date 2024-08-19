ac6c27d8dd4d228498ecbe975047fcec2db71419
Kamran <mkamran4786@gmail.com>
2024-08-19 09:47:28 +0500

    - add Browser and System Information

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {useState} from "react";
 import FingerprintJS from '@fingerprintjs/fingerprintjs';
+import {getPlugins} from "./utils/helper";
 
 function App() {
     const [data, setData] = useState({});
@@ -40,36 +41,109 @@ function App() {
             setLoading(false);
         }
     };
+
+    const BrowserPlugins = () => {
+        return getPlugins().map((plugin) => <div key={plugin.name}>
+            <ul>
+                <li>
+                    {`Name: ${plugin.name}`}
+                    <br />
+                    {`Description: ${plugin.description}`}
+                    <br />
+                    mimeTypes: <ul>
+                    {plugin.mimeTypes.map(e => <li>
+                        {`Suffixes: ${e.suffixes}`}
+                        <br />
+                        {`Type: ${e.type}`}
+                    </li>)}
+                </ul>
+                </li>
+            </ul>
+        </div>)
+    }
   return (
     <div className="App">
-      <button onClick={getFingerPrint}>
-        Get Finger Print
-      </button>
-        {showOutput && <div>
+        <div className="top-details">
+            <button onClick={getFingerPrint}>
+                Get Finger Print
+            </button>
+        </div>
+        {error && <div style={{color: 'red', fontWeight: '800'}}>
+            Something went wrong!
+            Please try again.
+        </div>}
+        {showOutput && !error && <div>
             {loading ? <div>Loading...</div> : <div className="out-put">
                 <br/>
-                <div className="info">
-                    <div className="title">Visitor Identifier Hash:</div>
-                    <div className="hash">{data.visitorId}</div>
-                </div>
-                <div className="separator"/>
-                <div className="info">
-                    <div className="title">Time took to get the identifier:</div>
-                    <div className="value">{time}ms</div>
-                </div>
-                <div className="info">
-                    <div className="title">Confidence Score:</div>
-                    <div className="value">{data.confidence?.score}</div>
-                </div>
-                <div className="info">
-                    <div className="title">User Agent:</div>
-                    <div className="value">{navigator?.userAgent}</div>
+                <div className="top-details">
+                    <div className="info">
+                        <div className="title">Visitor Identifier Hash:</div>
+                        <div className="hash">{data.visitorId}</div>
+                    </div>
+                    <div className="separator"/>
+                    <div className="info">
+                        <div className="title">Time took to get the identifier:</div>
+                        <div className="value">{time}ms</div>
+                    </div>
+                    <div className="info">
+                        <div className="title">Confidence Score:</div>
+                        <div className="value">{data.confidence?.score}</div>
+                    </div>
                 </div>
 
-                <div className="info">
-                    <div className="title">IP Address:</div>
-                    <div className="value">{ipAddress}</div>
+                <div className="separator"/>
+                <br />
+                <div className="section">
+                    <div className="section-label">
+                        Browser and System Information
+                    </div>
+                    <ul className="section-list">
+                        <li>
+                            <span className="list-label">User-Agent String:</span>
+                            <span className="list-value">{navigator?.userAgent}</span>
+                        </li>
+                        <li>
+                            <span className="list-label">Browser Plugins:</span>
+                            <span className="list-value"><BrowserPlugins/></span>
+                        </li>
+                        <li>
+                            <span className="list-label">Language and Locale:</span>
+                            <span className="list-value">{navigator?.languages.join(', ')}</span>
+                        </li>
+                        <li>
+                            <span className="list-label">Time Zone:</span>
+                            <span className="list-value">{Intl.DateTimeFormat().resolvedOptions().timeZone}</span>
+                        </li>
+                        <li>
+                            <span className="list-label">Screen Resolution:</span>
+                            <span className="list-value">
+                                <ul>
+                                    <li>
+                                        <span>Resolution:</span>
+                                        <span className="list-value">{`${window.screen.width} x ${window.screen.height} px`}</span>
+                                    </li>
+                                    <li>
+                                        <span>Color Depth:</span>
+                                        <span className="list-value">{window.screen.colorDepth}</span>
+                                    </li>
+                                    <li>
+                                        <span>Pixel Depth:</span>
+                                        <span className="list-value">{window.screen.pixelDepth}</span>
+                                    </li>
+                                </ul>
+                            </span>
+                        </li>
+                        <li>
+                            <span className="list-label">Window Size:</span>
+                            <span className="list-value">{`${window.innerWidth} x ${window.innerHeight} px`}</span>
+                        </li>
+                        <li>
+                            <span className="list-label">Platform:</span>
+                            <span className="list-value">{navigator?.platform}</span>
+                        </li>
+                    </ul>
                 </div>
+
             </div>}
         </div>}
     </div>
---
+++ src/utils/helper.js
@@ -0,0 +1,34 @@
+export const getPlugins = () => {
+    const rawPlugins = navigator.plugins
+
+    if (!rawPlugins) {
+        return undefined
+    }
+
+    const plugins = []
+
+    // Safari 10 doesn't support iterating navigator.plugins with for...of
+    for (let i = 0; i < rawPlugins.length; ++i) {
+        const plugin = rawPlugins[i]
+        if (!plugin) {
+            continue
+        }
+
+        const mimeTypes = []
+        for (let j = 0; j < plugin.length; ++j) {
+            const mimeType = plugin[j]
+            mimeTypes.push({
+                type: mimeType.type,
+                suffixes: mimeType.suffixes,
+            })
+        }
+
+        plugins.push({
+            name: plugin.name,
+            description: plugin.description,
+            mimeTypes,
+        })
+    }
+
+    return plugins
+}
